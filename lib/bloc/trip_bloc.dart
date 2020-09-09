a5fadedfe15b75739840a55ff6de3a00767fd061
Egor Volvachev <yahor.valvachou@ihsmarkit.com>
2020-09-09 21:06:33 +0300

    update trip bloc

--- lib/bloc/trip_bloc.dart
+++ lib/bloc/trip_bloc.dart
@@ -1,14 +1,24 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc/bloc.dart';
 import 'package:way/bloc/trip_event.dart';
 import 'package:way/bloc/trip_state.dart';
+import 'package:way/models/trip.dart';
+import 'package:way/services/trip_repository.dart';
 
 class TripBloc extends Bloc<TripEvent, TripState> {
-  @override
-  get initialState => {};
+  final TripRepository tripRepository;
+
+  TripBloc({this.tripRepository}) : super(TripEmptyState());
 
   @override
-  Stream mapEventToState(event) {
-    // TODO: implement mapEventToState
-    throw UnimplementedError();
+  Stream<TripState> mapEventToState(TripEvent event) async* {
+    if (event is TripLoadEvent) {
+      yield TripLoadingState();
+      try {
+        final List<Trip> _loadedTrips = await tripRepository.getAllTrip();
+        yield TripLoadedState(loadedTrips: _loadedTrips);
+      } catch (_) {
+        yield TripEmptyState();
+      }
+    }
   }
 }
